8baad89f27ef17878ac9ea582c879c6129d5385f
louiscklaw <louiscklaw@gmail.com>
2019-12-31 23:56:03 +0800

    update,

--- temp.sh
+++ temp.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env sh
 
-git clone --depth=1 https://github.com/reprappro/Ormerod.git _ref/reprappro/Ormerod
-git clone --depth=1 https://github.com/RepRapLtd/RepRap-MultiFilament.git _ref/RepRapLtd/RepRap-MultiFilament
-git clone --depth=1 https://github.com/reprappro/Paste-extruder.git _ref/reprappro/Paste-extruder
-git clone --depth=1 https://github.com/reprappro/Double-Mendel.git _ref/reprappro/Double-Mendel
-git clone --depth=1 https://github.com/reprappro/proximity.git _ref/reprappro/proximity
-git clone --depth=1 https://github.com/reprappro/fiveVreg.git _ref/reprappro/fiveVreg
-git clone --depth=1 https://github.com/reprappro/atx-power.git _ref/reprappro/atx-power
-git clone --depth=1 https://github.com/reprappro/Cable-chain.git _ref/reprappro/Cable-chain
-git clone --depth=1 https://github.com/reprappro/Quick-set-hot-end.git _ref/reprappro/Quick-set-hot-end
-git clone --depth=1 https://github.com/reprappro/Ormerod.git _ref/reprappro/Ormerod
-git clone --depth=1 https://github.com/RepRapLtd/FreeCADMacros.git _ref/RepRapLtd/FreeCADMacros
+git clone --depth=1 https://github.com/fessyfoo/fooyabt-belt-tensioner.git _ref/fessyfoo/fooyabt-belt-tensioner
